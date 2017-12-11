6fc5757efefc8b8765f8fdb5728899a14bb6d581
Clement Ramondou <cl.ramondou@gmail.com>
2017-12-11 01:28:11 +0100

    init

---
+++ js/script.js
@@ -0,0 +1,53 @@
+﻿//this function sends and receives the pin's status
+function change_state(pin, status) {
+    //this is the http request
+    var request = new XMLHttpRequest();
+    request.open("GET", "gpio.php?pin=" + pin + "&status=" + status);
+    request.send(null);
+    //receiving information
+    request.onreadystatechange = function () {
+        if (request.readyState == 4 && request.status == 200) {
+            return ('test');
+        }
+        //test if fail
+        else if (request.readyState == 4 && request.status == 500) {
+            alert("server error");
+            return ('fail');
+        }
+        //else
+        else {
+            return false;
+        }
+    }
+}
+
+//this function exec change_state() for selected item
+function gpio_selected(_this) {
+    var gpioNumber = parseInt($(_this).val());
+    if ($(_this).prop('checked')) {
+        var new_status = change_state(gpioNumber, 0);
+        if (new_status == false) {
+            console.log('Error: change_state() fail');
+            return false;
+        }
+    } else {
+        var new_status = change_state(gpioNumber, 1);
+        if (new_status == false) {
+            console.log('Error: change_state() fail');
+            return false;
+        }
+    }
+}
+
+$(function () {
+
+    $('li').click(function () {
+        var element = $(this).find('input');
+        gpio_selected(element);
+        if (element.prop('checked')) {
+            element.prop('checked', false);
+        }else{
+            element.prop('checked', true);
+        }
+    });
+});	
